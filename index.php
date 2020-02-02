58fe921e1815ac2cb053b390586adadce409e161
Scarlett Kim <scarlettkimm@gmail.com>
2020-02-02 16:32:04 -0500

    add validation for colors

--- index.php
+++ index.php
@@ -44,19 +44,25 @@ $f3->route('GET|POST /order', function($f3){
     echo $view->render('views/form1.html');
 });
 
-$f3->route('GET|POST /order2', function(){
+$f3->route('GET|POST /order2',
+    function($f3) {
+
+        if (isset($_POST['submit'])) {
+            $color = $_POST['color'];
+            if (validColor($color)) {
+                $_SESSION['color'] = $color;
+                $f3->reroute('/results');
+            } else {
+                $f3->set("errors['color']", "Please select a color.");
+            }
+        }
+
+        $template = new Template();
+        echo $template->render('views/form2.html');
+    });
+
+$f3->route('GET /results', function (){
     $view = new Template();
-
-    $_SESSION ['animal'] = $_POST['animal'];
-
-    echo $view->render('views/form2.html');
-});
-
-$f3->route('POST /results', function (){
-    $view = new Template();
-
-    $_SESSION ['color'] = $_POST['color'];
-
     echo $view->render('views/results.html');
 });
 
